44ec048f7fd188caf22e40a76cb4a4c35fad557e
secolema <seanclmn1@gmail.com>
2021-12-17 14:26:11 -0500

    work in progress...

--- funchat_front/src/components/chatroom_container/Chatroom_container.jsx
+++ funchat_front/src/components/chatroom_container/Chatroom_container.jsx
@@ -18,14 +18,21 @@ function Chatroom_container() {
         <div className='chatroom-container'>
             <MyProfile profilename = {"kumamon"} img_url={process.env.PUBLIC_URL+'/kumamon.jpeg'} />
             <SignOut/>
+
+            <nav className='chatroom-nav'>
+                <a className='chatroom-divider'>Chats</a>
+                <a className='chatroom-divider'>Explore</a>
+            </nav>
             <ChatroomSearch/>
 
-            <Chatroom chatroomname={"Qoo"} img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}/>
-            <Chatroom chatroomname={"Qoo"} img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}/>
-
-            <Chatroom chatroomname={"Qoo"} img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}/>
-
-            <Chatroom chatroomname={"Qoo"} img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}/>
+            <Chatroom 
+                chatroomname={"Qoo"} 
+                img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}
+            />
+            <Chatroom 
+                chatroomname={"Qoo"} 
+                img_url={process.env.PUBLIC_URL+'/qoo.jpeg'}
+            />
 
 
         </div>
---
+++ funchat_front/src/components/my-chatrooms/MyChatrooms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mychatrooms() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Mychatrooms
